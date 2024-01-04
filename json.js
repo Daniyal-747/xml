25b9b80df88c20216ab6b5abeb1b991a83e189b0
Daniyal Mirza <danial.aptech96@gmail.com>
2024-01-04 20:46:48 +0500

    JSON

---
+++ json.js
@@ -0,0 +1,21 @@
+// Curly brackets are objects and square brackets are Array.
+
+var organization = [
+
+    {
+        "id": 0,
+        "Employee Name": "Daniyal Mirza",
+        "Salary": "20,000",
+        "Department": "CSD",
+        "Designation": "Accounts Officer"
+    },
+
+    {
+        "id": 1,
+        "Employee Name": "Ahmed Khan",
+        "Salary": "50,000",
+        "Department": "CSD",
+        "Designation": "Assistant Manager"
+    },
+
+]
