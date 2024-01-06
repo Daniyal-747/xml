159557b00ed0786435354720e7cbe35d9c6ea7aa
Daniyal Mirza <danial.aptech96@gmail.com>
2024-01-06 20:10:30 +0500

    Comparison Products

---
+++ JSON/app.js
@@ -0,0 +1,28 @@
+// >${products[i].title} is showing the title of the products in each index
+
+// "${i}"
+
+for(let i=1; i<products.length; i++){
+    document.querySelector('#select1').innerHTML += `
+    <option value="${i}">${products[i].title}</option>`
+}
+
+for(let i=1; i<products.length; i++){
+    document.querySelector('#select2').innerHTML += `
+    <option value="${i}">${products[i].title}</option>`
+}
+
+function item1(a){
+    document.querySelector('#img1').src = products[a].image;
+    document.querySelector('#price1').innerHTML = products[a].price;
+    document.querySelector('#brand1').innerHTML = products[a].brand;
+    document.querySelector('#desc1').innerHTML = products[a].description;
+}
+
+function item2(a){
+    document.querySelector('#img2').src = products[a].image;
+    document.querySelector('#price2').innerHTML = products[a].price;
+    document.querySelector('#brand2').innerHTML = products[a].brand;
+    document.querySelector('#desc2').innerHTML = products[a].description;
+}
+
